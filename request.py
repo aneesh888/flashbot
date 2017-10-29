0098ed3c5529a15b2ba3fb98f8205b6b3a032d99
aneesh <aneesh.aneesh888@gmail.com>
2017-10-29 15:54:56 +0530

    request.py created

---
+++ request.py
@@ -0,0 +1,11 @@
+import requests
+
+
+class FlashGet(object):
+	# Sends a GET request
+	def __init__(url=None, headers=None, callback=None):
+		self.url = url
+		self.headers = headers
+		self.callback = callback
+
+	
