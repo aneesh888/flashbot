09e382ba7e573d8d170197dc4a7fab5d5a1de76c
aneesh <aneesh.aneesh888@gmail.com>
2017-10-29 15:56:06 +0530

    Converted to space

--- request.py
+++ request.py
@@ -2,10 +2,8 @@ import requests
 
 
 class FlashGet(object):
-	# Sends a GET request
-	def __init__(url=None, headers=None, callback=None):
-		self.url = url
-		self.headers = headers
-		self.callback = callback
-
-	
+    # Sends a GET request
+    def __init__(url=None, headers=None, callback=None):
+        self.url = url
+        self.headers = headers
+        self.callback = callback
